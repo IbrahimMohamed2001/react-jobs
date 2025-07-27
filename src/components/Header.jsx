55438b634116fc6c14d217b5eb79032084aa8d75
Ibrahim Mohamed <106034477+IbrahimMohamed2001@users.noreply.github.com>
2025-07-27 11:23:21 +0300

    adding header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+// import React from "react";
+// import reactLogo from "../assets/react.svg";
+const Header = () => {
+  return (
+    <>
+      <div className="text-center font-bold text-2xl w-full">Hello world</div>
+    </>
+  );
+};
+
+export default Header;
