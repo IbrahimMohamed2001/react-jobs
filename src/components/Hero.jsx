2bcbd0e3c8cbcd47a7efa4ddf53b9b6933d3bf91
IbrahimMohamed2001 <ibrahim.hassan01@eng-st.cu.edu.eg>
2025-07-30 00:00:55 +0300

    adding new section: "Hero"

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className="bg-indigo-700 mx-auto py-20 px-10">
+      <div className="container mx-auto flex flex-col items-center justify-center text-indigo-100 space-y-5">
+        <div className="font-extrabold text-2xl md:text-4xl lg:text-5xl">Become a React Developer</div>
+        <div className="text-lg md:text-2xl lg:text-3xl">Find <b>React Jobs</b> the fits your skills and needs</div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
