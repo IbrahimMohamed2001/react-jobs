08ee574b27f0f767007fa43cb456c7ef5dd21b90
IbrahimMohamed2001 <ibrahim.hassan01@eng-st.cu.edu.eg>
2025-07-30 00:01:10 +0300

    adding it to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 const App = () => {
   return <>
     <Header />
+    <Hero />
   </>
 };
 
