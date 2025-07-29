5fbbea35338093e39ed9c6da96849f4f20a391ec
IbrahimMohamed2001 <ibrahim.hassan01@eng-st.cu.edu.eg>
2025-07-30 00:06:31 +0300

    using gradient colors instead

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@
 
 const Hero = () => {
   return (
-    <section className="bg-indigo-700 mx-auto py-20 px-10">
+    <section className="bg-gradient-to-l from-indigo-700 via-indigo-600 to-indigo-700 mx-auto py-20 px-10">
       <div className="container mx-auto flex flex-col items-center justify-center text-indigo-100 space-y-5">
         <div className="font-extrabold text-2xl md:text-4xl lg:text-5xl">Become a React Developer</div>
         <div className="text-lg md:text-2xl lg:text-3xl">Find <b>React Jobs</b> the fits your skills and needs</div>
