9e66c8f14181c98aae0d96586480c7e575dc3ac6
Tis-Control-HA <tishomeassistant@gmail.com>
2025-08-02 02:46:49 +0300

    choosing better colors

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -12,7 +12,7 @@ const HomeCards = () => {
           <a href="#" className='text-indigo-50 self-start duration-300 transition-all rounded-md font-bold px-4 py-3 bg-slate-950 hover:bg-slate-700'>Browse Jobs</a>
         </div>
 
-        <div className="rounded-md bg-indigo-200 px-6 py-5 flex flex-col space-y-6 w-full duration-500 transition-all drop-shadow-md md:w-1/2 hover:bg-indigo-100">
+        <div className="rounded-md bg-indigo-100 px-6 py-5 flex flex-col space-y-6 w-full duration-500 transition-all drop-shadow-md md:w-1/2 hover:bg-indigo-50">
           <div className="flex flex-col space-y-1">
             <div className="text-blac k text-2xl font-bold">For Employers</div>
           <div className="text-black text-sm font-bold">List your job to find the perfect developer for the role</div>
