0918d48cfdd139d41ed0920af561b8a1415a92ec
Tis-Control-HA <tishomeassistant@gmail.com>
2025-08-04 01:06:20 +0300

    adding further stylings for more better looking

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -10,16 +10,20 @@ const JobListings = () => {
           </div>
 
           <div className="flex flex-col items-start px-5 justify-center rounded-md bg-white drop-shadow-md py-3 space-y-3">
-            <div className="flex flex-col space-y-1">
+            <div className="flex flex-col w-full space-y-0.5">
               <h4 className="text-gray-400 font-bold text-sm">Full-time</h4>
               <h2 className="text-slate-900 text-2xl font-bold">Senior React Developer</h2>
             </div>
-            <p className="text-slate-900 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, reprehenderit! Ipsum tempore quas autem atque. Eum voluptatem quas accusamus earum assumenda molestias non, cumque obcaecati dicta? Praesentium veritatis rem voluptatum.</p>
-            <p className="text-indigo-400 text-xs font-bold">$70 - $80 K/Year</p>
-            <hr className="border-1 bg-gray-500 w-full"/>
-            <p className="text-red-600 font-bold text-xs">Boston, MA</p>
-            <a href="#" className="text-white bg-indigo-600 w-full text-center rounded-md py-1 transition-all duration-300 hover:bg-indigo-500">Read More</a>
+            <p className="text-slate-900 text-xs font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, reprehenderit! Ipsum tempore quas autem atque. Eum voluptatem quas accusamus earum assumenda molestias non, cumque obcaecati dicta? Praesentium veritatis rem voluptatum.</p>
+            <div className="w-full flex flex-col space-y-1">
+              <p className="text-indigo-500 text-xs font-bold">$70 - $80 K/Year</p>
+              <hr className="border-1 bg-gray-500 w-full"/>
 
+            </div>
+            <div className="flex flex-col w-full space-y-3 pb-3">
+              <p className="text-red-700 font-bold text-xs">Boston, MA</p>
+              <a href="#" className="text-white bg-indigo-600 w-full text-center rounded-md py-1 transition-all duration-300 hover:bg-indigo-500">Read More</a>
+            </div>
           </div>
         </div>
       </div>
