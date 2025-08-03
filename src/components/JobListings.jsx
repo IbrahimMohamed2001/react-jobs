8cff954b74357c606f046a226c6d836ba3a3440c
Tis-Control-HA <tishomeassistant@gmail.com>
2025-08-04 00:13:20 +0300

    adding job listings

---
+++ src/components/JobListings.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const JobListings = () => {
+  return (
+    <section id="job-listings" className="w-full bg-indigo-50">
+      <div className="container mx-auto py-4">
+        <div className="flex flex-col items-center justify-center space-y-5">
+          <div className="text-center text-2xl text-indigo-500 font-bold">
+            Browse Jobs
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default JobListings
