c834dc1c8f23caeb673ade81f10d75fcf04f5a12
IbrahimMohamed2001 <ibrahim.hassan01@eng-st.cu.edu.eg>
2025-08-05 15:42:51 +0300

    separate between home page and jobs page

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import JobCard from './JobCard';
 import jobs from '../jobs.json';
 
-const JobListings = () => {
-  const recentJobs = jobs.slice(0, 3);
+const JobListings = ({isHome = false}) => {
+  const jobListings = isHome ? jobs.slice (0, 3) : jobs;
   return (
     <section id="job-listings" className="w-full bg-indigo-50">
       <div className="container mx-auto py-4">
         <div className="flex flex-col items-center justify-center space-y-5 px-5">
           <div className="text-center text-2xl text-indigo-500 font-bold md:text-3xl">
-            Browse Jobs
+            {isHome ? 'Recent Jobs' : 'Browse Jobs'}
           </div>
 
           <div className="grid grid-cols-1 gap-y-3 md:grid-cols-2 md:gap-x-3 lg:grid-cols-3">
-            {recentJobs.map (item => (
+            {jobListings.map (item => (
               <JobCard
                 key={item.id}
                 title={item.title}
