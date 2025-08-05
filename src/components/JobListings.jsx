3cd9a781c3eb9c8dd73cb3cbd2a4daed811a4bac
IbrahimMohamed2001 <ibrahim.hassan01@eng-st.cu.edu.eg>
2025-08-05 18:38:55 +0300

    fetching data from the json server

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 import JobCard from './JobCard';
-import jobs from '../jobs.json';
 
 const JobListings = ({isHome = false}) => {
-  const jobListings = isHome ? jobs["jobs"].slice (0, 3) : jobs["jobs"];
+  const [jobs, setJobs] = useState ([]);
+  const [loading, setLoading] = useState (true);
+
+  useEffect (() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch ('http://localhost:9000/jobs');
+        const data = await res.json ();
+        setJobs (data);
+      } catch (error) {
+        console.log ('error fetching data', error);
+      } finally {
+        setLoading (false);
+      }
+    };
+
+    fetchData ();
+  }, []);
+
   return (
     <section id="job-listings" className="w-full bg-indigo-50">
       <div className="container mx-auto py-4">
@@ -13,17 +30,19 @@ const JobListings = ({isHome = false}) => {
           </div>
 
           <div className="grid grid-cols-1 gap-y-3 md:grid-cols-2 md:gap-x-3 lg:grid-cols-3">
-            {jobListings.map (item => (
-              <JobCard
-                key={item.id}
-                title={item.title}
-                type={item.type}
-                description={item.description}
-                location={item.location}
-                salary={item.salary}
-                href={`/jobs/${item.id}`}
-              />
-            ))}
+            {loading
+              ? <h2>Loading...</h2>
+              : jobs.map (item => (
+                  <JobCard
+                    key={item.id}
+                    title={item.title}
+                    type={item.type}
+                    description={item.description}
+                    location={item.location}
+                    salary={item.salary}
+                    href={`/jobs/${item.id}`}
+                  />
+                ))}
           </div>
         </div>
       </div>
