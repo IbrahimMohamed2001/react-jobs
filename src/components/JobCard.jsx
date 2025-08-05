38495d7d5392ac51a1b783e1913f8111e2955cd7
IbrahimMohamed2001 <ibrahim.hassan01@eng-st.cu.edu.eg>
2025-08-05 08:10:14 +0300

    adding JobCard component and use it inside JobListings one

---
+++ src/components/JobCard.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const JobCard = ({
+  rule = 'Senior React Developer',
+  type = 'Full-Time',
+  description = 'Some thing',
+  salary = '$70 - $80 K/Year',
+  location = 'Boston, MA',
+}) => {
+  return (
+    <div className="flex flex-col items-start px-5 
+                            justify-center rounded-md 
+                            bg-white drop-shadow-md py-3 
+                            space-y-3 transition-all 
+                            duration-300 hover:bg-slate-50 
+                            hover:shadow-indigo-300 
+                            hover:shadow-lg hover:-translate-y-1">
+      <div className="flex flex-col w-full space-y-0.5">
+        <h4 className="text-gray-400 font-bold text-sm">{type}</h4>
+
+        <h2 className="text-slate-900 text-2xl font-bold">
+          {rule}
+        </h2>
+      </div>
+
+      <p className="text-slate-900 text-xs font-semibold">
+        {description}
+      </p>
+
+      <div className="w-full flex flex-col space-y-1">
+        <p className="text-indigo-500 text-xs font-bold pt-1">
+          {salary}
+        </p>
+
+        <hr className="border-1 bg-gray-500 w-full" />
+      </div>
+
+      <div className="flex flex-col w-full space-y-3 pb-3">
+        <p className="text-red-700 font-bold text-xs">{location}</p>
+
+        <a
+          href="#"
+          className="text-white bg-indigo-600 w-full text-center
+                      rounded-md py-1 transition-all duration-300 
+                      hover:bg-indigo-500"
+        >
+          Read More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default JobCard;
