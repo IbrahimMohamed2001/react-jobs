a1565e046e9654c45249aa3017f52706ceb3d891
IbrahimMohamed2001 <ibrahim.hassan01@eng-st.cu.edu.eg>
2025-08-05 12:30:44 +0300

    adding Outlet components which is as the children of this component

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import { Outlet } from 'react-router-dom';
 
-const MainLayout = ({children}) => {
+const MainLayout = () => {
   return (
     <>
       <Header />
 
-      {children}
+      <Outlet />
     </>
   );
 };
