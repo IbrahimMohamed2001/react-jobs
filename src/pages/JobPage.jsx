8935786bcf840b53c23259c0fe7166e0fa3e8a51
IbrahimMohamed2001 <ibrahim.hassan01@eng-st.cu.edu.eg>
2025-08-07 18:17:36 +0300

    last touches

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -6,7 +6,7 @@ const JobPage = () => {
   const job = useLoaderData ();
 
   return (
-    <div className="w-full mx-auto bg-indigo-50">
+    <div className="w-full mx-auto min-h-screen bg-indigo-50">
       <div className="flex flex-col items-center justify-center">
         <div className="w-full bg-white px-7 py-3">
           <Link
@@ -18,65 +18,72 @@ const JobPage = () => {
         </div>
 
         <div className="px-7 py-5 mx-auto w-full">
-          <div className="grid grid-rows-3 ">
-            <div class="grid grid-flow-col grid-cols-3 gap-5">
-              <div class="col-span-2 bg-white rounded-md px-6 py-3 drop-shadow-md">
-                <div className="flex flex-col items-start justify-center space-y-3">
-                  <div className="text-sm font-bold text-gray-400">
-                    {job.type}
-                  </div>
-                  <div className="text-md font-bold text-black md:text-xl lg:text-2xl">
-                    {job.title}
-                  </div>
-                  <p className="text-red-700 font-bold text-xs pb-5">
-                    <FaMapMarker className="inline text-lg mr-1" />
-                    {job.location}
-                  </p>
+          <div class="grid grid-flow-col grid-cols-3 gap-5">
+            <div class="col-span-2 bg-white rounded-md px-6 py-3 drop-shadow-md">
+              <div className="flex flex-col items-start justify-center space-y-3">
+                <div className="text-sm font-bold text-gray-400">
+                  {job.type}
+                </div>
+                <div className="text-md font-bold text-black md:text-xl lg:text-2xl">
+                  {job.title}
+                </div>
+                <p className="text-red-700 font-bold text-xs">
+                  <FaMapMarker className="inline text-lg mr-1" />
+                  {job.location}
+                </p>
+              </div>
+            </div>
+            <div class="col-span-2 bg-white rounded-md px-6 py-3 drop-shadow-md">
+              <div className="flex flex-col items-start justify-center space-y-3">
+                <div className="text-indigo-700 font-bold text-md">
+                  Job Description
+                </div>
+                <div className="text-sm">
+                  {job.description}
+                </div>
+                <hr className="border-1 bg-gray-500 w-full" />
+                <div className="text-indigo-700 font-bold text-md">
+                  Salary
+                </div>
+                <div className="text-sm">
+                  {job.salary} / Year
                 </div>
               </div>
-              <div class="col-span-2 bg-white rounded-md px-6 py-3 drop-shadow-md">
-                <div className="flex flex-col items-start justify-center space-y-3">
-                  <div className="text-indigo-700 font-bold text-md">
-                    Job Description
-                  </div>
-                  <div className="text-sm">
-                    {job.description}
-                  </div>
-                  <div className="text-indigo-700 font-bold text-md pt-5">
-                    Salary
-                  </div>
-                  <div className="text-sm">
-                    {job.salary} / Year
-                  </div>
+            </div>
+            <div class="row-span-2 bg-white rounded-md px-3 py-3 drop-shadow-md">
+              <div className="flex flex-col items-start justify-center space-y-3">
+                <div className="text-indigo-700 font-bold text-md">
+                  Company Info
                 </div>
-              </div>
-              <div class="row-span-2 bg-white rounded-md px-3 py-3 drop-shadow-md">
-                <div className="flex flex-col items-start justify-center space-y-3">
-                  <div className="text-indigo-700 font-bold text-md">
-                    Company Info
-                  </div>
 
-                  <div className="text-md md:text-lg">
-                    {job.company.name}
-                  </div>
+                <div className="text-md md:text-lg">
+                  {job.company.name}
+                </div>
 
-                  <div className="text-xs md:text-sm">
-                    {job.company.description}
-                  </div>
+                <div className="text-xs md:text-sm">
+                  {job.company.description}
+                </div>
 
-                  <hr className="border-1 bg-gray-500 w-full" />
+                <hr className="border-1 bg-gray-500 w-full" />
 
-                  <div className="text-md">Contact Email:</div>
+                <div className="text-indigo-700 font-bold text-md">
+                  Contacts
+                </div>
 
+                <div className="w-full">
+                  <div className="text-md md:text-sm pb-1">Email</div>
                   <div className="bg-indigo-200 rounded-sm font-bold text-xs w-full px-2 py-2">
                     {job.company.contactEmail}
                   </div>
-                  <div className="text-md">Contact Phone:</div>
+                </div>
+
+                <div className="w-full">
+                  <div className="text-md md:text-sm pb-1">Phone</div>
                   <div className="bg-indigo-200 rounded-sm font-bold text-xs w-full px-2 py-2">
                     {job.company.contactPhone}
                   </div>
-                  <div></div>
                 </div>
+                <div />
               </div>
             </div>
           </div>
