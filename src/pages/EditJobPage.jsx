b94799edec9eb300aaa79a60cf1a9542f376d9f9
IbrahimMohamed2001 <ibrahim.hassan01@eng-st.cu.edu.eg>
2025-08-12 16:34:15 +0300

    added a psuedo function of editing and pass it to the job form component to act like submit function

--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -7,10 +7,18 @@ const EditJobPage = () => {
   return (
     <section className="bg-indigo-50 min-h-screen w-full mx-auto">
       <div className="contianer w-full mx-auto flex items-center justify-center px-10 py-16">
-        <JobForm submitButtonTitle='Edit Job' job={job} />
+        <JobForm
+          submitButtonTitle="Edit Job"
+          job={job}
+          submitFunction={editJob}
+        />
       </div>
     </section>
   );
+};
+
+const editJob = () => {
+  console.log ('editing the job');
 };
 
 export default EditJobPage;
