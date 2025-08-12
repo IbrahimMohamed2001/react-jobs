8452c8c9776b3bc443a7e41d81112f0dab25138d
IbrahimMohamed2001 <ibrahim.hassan01@eng-st.cu.edu.eg>
2025-08-12 23:55:20 +0300

    adding toast container to the layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from '../components/Header';
-import { Outlet } from 'react-router-dom';
+import {Outlet} from 'react-router-dom';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/ReactToastify.css';
 
 const MainLayout = () => {
   return (
     <>
       <Header />
-
       <Outlet />
+      <ToastContainer />
     </>
   );
 };
