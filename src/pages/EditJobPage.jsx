ab67aa4d0712494fb944532efe4c9ec7d2dc51e1
IbrahimMohamed2001 <ibrahim.hassan01@eng-st.cu.edu.eg>
2025-08-12 16:25:28 +0300

    adding the form inside the edit job page

--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import {useLoaderData} from 'react-router-dom';
+import JobForm from '../components/JobForm';
 
 const EditJobPage = () => {
-  return <div>EditJobPage</div>;
+  const job = useLoaderData ();
+
+  return (
+    <section className="bg-indigo-50 min-h-screen w-full mx-auto">
+      <div className="contianer w-full mx-auto flex items-center justify-center px-10 py-16">
+        <JobForm submitButtonTitle='Edit Job' job={job} />
+      </div>
+    </section>
+  );
 };
 
 export default EditJobPage;
